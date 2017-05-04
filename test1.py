ef794ffe939d1acb3a28397907d1dc00a886b5fd
Mattias Hallberg <mattias.hallberg@smarteye.se>
2017-05-04 14:32:12 +0200

    Normalizing and added sine tones for testing.

--- test1.py
+++ test1.py
@@ -5,28 +5,35 @@ import wave
 import util
 import filter
 
-wavfile = wave.open("./testfiles/Grand Piano - Fazioli - minor chords - Am highest.wav")
-# wavfile = wave.open("./testfiles/Grand Piano - Fazioli - minor chords - Gm highest.wav")
-print("Sampling freq={0}, noOfChannels={1}".format(wavfile.getframerate(), wavfile.getnchannels()))
+# wavfile = wave.open("./testfiles/Grand Piano - Fazioli - minor chords - Am highest.wav")
+#wavfile = wave.open("./testfiles/Grand Piano - Fazioli - minor chords - Gm highest.wav")
+wavfile = wave.open("./testfiles/440Hz_44100Hz_16bit_05sec.wav")
+
+fs = wavfile.getframerate()
+nchannels = wavfile.getnchannels()
+
+print("Sampling freq={0}, noOfChannels={1}".format(fs, nchannels))
 
 # Plot wav file
 signal = wavfile.readframes(-1)
 signal = np.fromstring(signal, 'Int16')
-mono = util.create_mono_stream_float(signal)
+
+if nchannels > 1:
+    signal = util.create_mono_stream_float(signal)
 
 # Get time from indices
-fs = wavfile.getframerate()
-Time=np.linspace(0, len(mono)/fs, len(mono))
+#fs = wavfile.getframerate()
+#Time=np.linspace(0, len(mono)/fs, len(mono))
 # Plot
-plt.figure(1)
-plt.title('Signal Wave...')
-plt.plot(Time, mono)
+#plt.figure(1)
+#plt.title('Signal Wave...')
+#plt.plot(Time, mono)
 # plt.show()
 
 # Re-sample signal at 11025 Hz (Maxing out at 5512.5 Hz)
-mono = mono[::4]
+signal = signal[::4]
 fs = 11025
-mono = filter.butter_lowpass_filter(mono, 5000, fs, order=5)
+signal = filter.butter_lowpass_filter(signal, 5000, fs, order=5)
 
 # FFT test signal
 N = fs #fs//2
@@ -39,12 +46,26 @@ T = 1.0 / fs
 #    y = channels[0][i*N:i*N+N]
     # Number of samplepoints
     # sample spacing
-yf = scipy.fftpack.fft(mono[:N])
+yf = scipy.fftpack.fft(signal[:N])
 xf = np.linspace(0.0, 1.0/(2.0*T), N//2)
 fig, ax = plt.subplots()
 ax.plot(xf, 2.0/N * np.abs(yf[0:N//2]))
-#plt.show()
+plt.show()
 
 # Use the first half of the data and convert to magnitude
 X = np.abs(yf[0:len(yf)//2])
-print("Done")
+print(type(X))
+
+# Normalize X
+X = X/sum(X)
+
+# Save the 20 last and reverse
+sorted1 = X.argsort()[-100:][::-1]
+
+area = sum(X[sorted1])
+print("Area of the 100 first : {}".format(area))
+
+for i in range(0, len(sorted1)):
+    print("Frequency: {0} Area: {1}".format(sorted1[i], X[sorted1[i]]))
+
+#print(sorted1*(fs/N))
