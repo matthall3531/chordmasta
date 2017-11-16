90acadaf994e1ae805ddd9d4c5396b86ee251bbc
Mattias <matthall3531@gmail.com>
2017-11-16 19:25:07 +0100

    Removed unused imports.

--- src/se/softcoded/chordmasta/signalprocessing/FFT.java
+++ src/se/softcoded/chordmasta/signalprocessing/FFT.java
@@ -1,13 +1,9 @@
 package se.softcoded.chordmasta.signalprocessing;
 
-import org.junit.Test;
 import se.softcoded.chordmasta.*;
-import se.softcoded.chordmasta.test.TestTools;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.junit.Assert.assertEquals;
 
 public class FFT implements ProcessUnit {
     @Override
